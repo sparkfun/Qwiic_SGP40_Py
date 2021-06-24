2102708215e8a6e4ea2906e24e07f554a42666b1
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2021-06-24 11:31:27 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, SparkFun Electronics'
 author = 'SparkFun Electronics'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.01'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
